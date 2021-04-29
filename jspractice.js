3aafcc567bca8b76a7578347a93a05d6f5d3120b
laurennrmoore <lmooreintech2021@gmail.com>
2021-04-28 20:04:07 -0400

    jsalgdatastrhwpush

--- jspractice.js
+++ jspractice.js
@@ -1,19 +1,91 @@
-//DOM Manipulation: Selecting & changing website elements
+Sort an Array Alphabetically using the sort Method
+function alphabeticalOrder(arr) {
+  
+  
+  return arr.sort(function(a,b) {
+    return a === b ? 0 : a < b ? -1 : 1;
+  });
+  
+  
+  }
+  alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
 
-var div1 = document.getElementById('div1');
 
-var unicycle = document.getElementsByClassName('unicycle');
 
-var paragraphps = document.getElementsByTagName('p');
+  Return a Sorted Array Without Changing the Original Array
+  var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  return [].concat(arr).sort(function(a,b){
+  return a-b;
+});
+  
+}
+nonMutatingSort(globalArray);
 
-var queryUnicycle = document.querySelector('div2');
 
-var queryAll = document.querySelectorAll('.unicycle, #div2');
 
-var text = "<h1>Hello World<h1>";
+Split a String into an Array Using the split Method
+function splitify(str) {
+   
+  return str.split(/\W/);
+  
+  
+  }
+  splitify("Hello World,I-am code");
 
-queryUnicycle.textContent = text;
 
-// for (i=0; i < queryAll.length; ++i) {
-//     queryAll[i].innerHTML = text;
-// }
+
+  Combine an Array into a String Using the join Method
+  function sentensify(str) {
+    
+  return str.split(/\W/).join(" ");
+      
+  }
+  sentensify("May-the-force-be-with-you")
+
+
+
+  Apply Functional Programming to Convert Strings to URL Slugs
+  
+function urlSlug(title) {
+    return title 
+    .toLowerCase()
+    .trim()
+    .split(/\s+/)
+    .join("-");
+    
+    }
+    
+
+
+    Use the every Method to Check that Every Element in an Array Meets a Criteria
+    function checkPositive(arr) {
+       
+        return arr.every(function(value){
+        return value > 0;
+      });
+            
+             }
+      checkPositive([1, 2, 3, -4, 5]);
+
+
+
+      Use the some Method to Check that Any Elements in an Array Meet a Criteria
+      function checkPositive(arr) {
+      
+      return arr.some(elem => elem > 0);
+    }
+      checkPositive([1, 2, 3, -4, 5]);
+
+
+      Introduction to Currying and Partial Application
+      function add(x) {
+        
+      return function(y){
+        return function(z){
+          return x + y + z;
+        };
+      };
+      
+      }
+      add(10)(20)(30);
